0e33d30a20a72d263474ae4d7c573fc452ca55ed
berkterek <42406263+berkterek@users.noreply.github.com>
2021-01-22 12:24:57 +0300

    Platform Switch
    Unity game platform changed to android

---
+++ AlgebraGlobalPrototype/Assets/GameFolders/Tests/PlayModeTests/a_moving_cube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace PlayModeTests
+{
+    public class a_moving_cube
+    {
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator moving_forward_changes_position()
+        {
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = Vector3.zero;
+
+            for (int i = 0; i < 10; i++)
+            {
+                cube.transform.position += Vector3.forward;
+                yield return new WaitForSeconds(0.5f);
+            }
+
+            Assert.AreEqual(10f,cube.transform.position.z);
+            
+            yield return null;
+        }
+    }
+}
