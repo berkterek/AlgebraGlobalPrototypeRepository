2a4dd2285e8b660d2d7fdfea21720c48d32ee54a
berkterek <42406263+berkterek@users.noreply.github.com>
2021-01-22 11:14:28 +0300

    GameManager Added
    GameManager ve Utilities dosyasina singleton yapicak abstract class eklkendi

---
+++ AlgebraGlobalPrototype/Assets/GameFolders/Scripts/Abstracts/Utilities/SingletonObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlgebraGlobalPrototype.Abstracts.Utilities
+{
+    public abstract class SingletonObject<T> : MonoBehaviour where T : Component
+    {
+        public static T Instance { get; private set; }
+
+        protected void SingletonThisObject(T instance)
+        {
+            if (Instance == null)
+            {
+                Instance = instance;
+                DontDestroyOnLoad(this.gameObject);
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }    
+}
---
+++ AlgebraGlobalPrototype/Assets/GameFolders/Scripts/Concretes/Managers/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using AlgebraGlobalPrototype.Abstracts.Utilities;
+using UnityEngine;
+
+namespace AlgebraGlobalPrototype.Managers
+{
+    public class GameManager : SingletonObject<GameManager>
+    {
+        private void Awake()
+        {
+            SingletonThisObject(this);
+        }
+        
+        
+    }    
+}
+
